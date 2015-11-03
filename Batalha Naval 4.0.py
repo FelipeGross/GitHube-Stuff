b01d3fab8deee068abb18eb15330d9ed5126f36e
FelipeGross <fegross18@gmail.com>
2015-11-03 18:40:02 -0200

    Versão Final

--- Batalha Naval 4.0.py
+++ Batalha Naval 4.0.py
@@ -1,13 +1,6 @@
 from random import randint
 import copy
 
-tamanho_barco = {6:6,
-                 5:5,
-                 4:4,
-                 3:3,
-                 2:2,
-                 1:1}
-
 n = 0
 while n < 6:
         n = int(input("Digite um numero para o total de linhas e colunas. Lembre-se: Quanto maior o \nnumero, maior a dificuldade do jogo.\n\nOBS: O numero deve ser maior do que 6.\n"))
@@ -15,26 +8,45 @@ while n < 6:
 print ("\n-----------------------------------------------------\n")
 mapa = []
 
+tamanho_barco = open('c_barcos.docx', 'r')
+#pontos_barcos = open('p_barcos.docx', 'r')
+pontos_barcos = {6:15, 5:10, 4:7, 3:5, 2:3, 1:1}
 
- 
-pontos_barcos = {6:15,
-                 5:10,
-                 4:7,
-                 3:5,
-                 2:3,
-                 1:1}
-
+'''
+for item in tamanho_barco:
+    if item == 1:
+        t1 = 1
+    elif item == 2:
+        t2 = 2
+    elif item == 3:
+        t3 = 3
+    elif item == 4:
+        t4 = 4
+    elif item == 5:
+        t5 = 5
+    elif item == 6:
+        t6 = 6
+'''
 placar = 0
 
-for x in range(n):  
-    mapa.append(["~"] * n)
+z = []
+
+for j in range(n):
+   z.append(j+1)
+
+s = 0
+
+for x in range(n):
+    mapa.append (["~"] * n)
+    
 mapa_pc = mapa
 
-def print_mapa(mapa):
+def print_mapa(s, mapa):
     for row in mapa:
-        print (" ".join(row))
-        
+        print (z[s] , " ".join(row))
+        s += 1
 print ("Batalha Naval\n\nBem Vindo!\n\nHa 6 navios no mapa abaixo.\nCada um tem sua medida variando de 1 a 6 pontos no mapa.\n\nSe voce acertar todos: PARABENS! VOCE GANHOU O JOGO!\n\nBoa Sorte!\n\nInsira um valor de 1 a", n,"para linhas e pressione a tecla Enter.\nInsira um valor de 1 a", n, "para colunas e aperte Enter.\n\nTenha um bom divertimento!\n")
+
 if n*n > n*5 + 21:
     print ("Lembre-se: Voce so podera errar",n*5, "vezes.\n")
 
@@ -68,7 +80,7 @@ def navio_inteiro(tamanho, mapa_pc):
             else:
                 while x[1]+tamanho > n:
                     x=random_tudo(mapa)
-                if mapa_pc1[x[0]+j][x[1]] == "~":
+                if mapa_pc1[x[0]][x[1]+j] == "~":
                     mapa_pc1[x[0]][x[1]+j] = tamanho
                 else:
                     return navio_inteiro(tamanho,mapa_pc)
@@ -91,51 +103,56 @@ def posicao(navios):
         
         
 mapa_pc = posicao(navios)
-#print(mapa_pc)
+print(mapa_pc)
 
-erro = 0
+erro = n*5
 rodada = 1
 acertos = 0
 
-while erro < n*5:
+while erro > 0 :
     
     if placar == 41:
         print ("\nPARABENS! VOCE GANHOU O JOGO!\n")
         break    
 
     print ("\nRodada", rodada,"    Placar:",placar,"\n")
-    print_mapa(mapa)
+    print_mapa(s, mapa)
             
     l_guess = int(input("\nLinha:\n")) -1
+    while l_guess < 0 or l_guess > n-1:
+        print ("\nFora da area! Jogue novamente\n")
+        l_guess = int(input("\nLinha:\n")) -1
+        
     c_guess = int(input("Coluna:\n")) - 1
+    while c_guess < 0 or c_guess > n-1:
+        print ("\nFora da area! Jogue novamente\n")    
+        c_guess = int(input("Coluna:\n")) - 1
+        
     print ("\n-----------------------------------------------------")
     print ("Rodada",rodada, "    Placar:",placar)
 
-    if l_guess < 0 or l_guess > n-1 or c_guess < 0 or c_guess > n-1:
-        print ("\nFora da area! Jogue novamente\n")
-
-    elif(mapa[l_guess][c_guess] == "X" or mapa[l_guess][c_guess] == "o"):
-            print ("\nJa atirou ai! Jogue novamente\n")
-    
+    if(mapa[l_guess][c_guess] == "X" or mapa[l_guess][c_guess] == "o"):
+            print ("\nJa atirou ai! Jogue novamente")
+            
     else:    
         if mapa_pc[l_guess][c_guess] == "~":
-            print ("\nAgua!\n")
+            print ("\nAgua!")
             mapa[l_guess][c_guess] = "o"
-            erro += 1
+            erro -= 1
     
         elif mapa_pc[l_guess][c_guess] != "~":
-            print ("\nAcertou meu navio!\n")
+            print ("\nAcertou meu navio!")
             barco = mapa_pc[l_guess][c_guess]
             tamanho_barco[barco] -= 1
             
             if tamanho_barco[barco]==0:
-                print ("\nAfundou meu navio!\n")
+                print ("\nAfundou meu navio!")
                 placar += pontos_barcos[barco]
             mapa[l_guess][c_guess] = "X"
     
-    print ("Total de Erros:",erro)
+        if n*n > n*5 + 21:    
+            print ("\nChances de Errar:",erro)
     print ("-----------------------------------------------------\n")
     rodada += 1
     
 print ("\nFIM DE JOGO!\n")
-
--- Batalha Naval.py
+++ Batalha Naval.py
@@ -1,6 +1,24 @@
 from random import randint
 import copy
 
+n = 0
+while n < 6:
+        n = int(input("Digite um número para o total de linhas e colunas. Lembre-se: Quanto maior o \nnúmero, maior a dificuldade do jogo.\n\nOBS: O número deve ser maior do que 6.\n"))
+            
+'''    
+    try:
+        n = int(input("Digite um numero para o total de linhas e colunas. Lembre-se: Quanto maior o \nnumero, maior a dificuldade do jogo.\n\nOBS: O numero deve ser maior do que 6.\n"))
+    except:
+        print("use algarismos inteiros")
+        try:
+            n = int(input("Digite um numero para o total de linhas e colunas. Lembre-se: Quanto maior o \nnumero, maior a dificuldade do jogo.\n\nOBS: O numero deve ser maior do que 6.\n"))
+        except:
+            n = 6'''
+print ("\n-----------------------------------------------------\n")
+mapa = []
+
+#pontos_barcos = open('p_barcos.docx', 'r')
+
 tamanho_barco = {6:6,
                  5:5,
                  4:4,
@@ -8,15 +26,6 @@ tamanho_barco = {6:6,
                  2:2,
                  1:1}
 
-n = 0
-while n < 6:
-        n = int(input("Digite um numero para o total de linhas e colunas. Lembre-se: Quanto maior o \nnumero, maior a dificuldade do jogo.\n\nOBS: O numero deve ser maior do que 6.\n"))
-        
-print ("\n-----------------------------------------------------\n")
-mapa = []
-
-
- 
 pontos_barcos = {6:15,
                  5:10,
                  4:7,
@@ -26,21 +35,30 @@ pontos_barcos = {6:15,
 
 placar = 0
 
-for x in range(n):  
-    mapa.append(["~"] * n)
+z = []
+
+for g in range(n):
+   z.append(g+1)
+
+s = 0
+
+for f in range(n):
+    mapa.append (["~"] * n)
+  
 mapa_pc = mapa
 
-def print_mapa(mapa):
+def print_mapa(s, mapa):
     for row in mapa:
-        print (" ".join(row))
-        
-print ("Batalha Naval\n\nBem Vindo!\n\nHa 6 navios no mapa abaixo.\nCada um tem sua medida variando de 1 a 6 pontos no mapa.\n\nSe voce acertar todos: PARABENS! VOCE GANHOU O JOGO!\n\nBoa Sorte!\n\nInsira um valor de 1 a", n,"para linhas e pressione a tecla Enter.\nInsira um valor de 1 a", n, "para colunas e aperte Enter.\n\nTenha um bom divertimento!\n")
+        print (" ".join(row), z[s])
+        s += 1
+print ("Batalha Naval\n\nBem Vindo!\n\nHá 6 navios no mapa abaixo.\nCada um tem sua medida variando de 1 a 6 pontos no mapa.\n\nSe você acertar todos: PARABÉNS! VOCÊ GANHOU O JOGO!\n\nBoa Sorte!\n\nInsira um valor de 1 a", n,"para linhas e pressione a tecla Enter.\nInsira um valor de 1 a", n, "para colunas e aperte Enter.\n\nTenha um bom divertimento!\n")
+
 if n*n > n*5 + 21:
-    print ("Lembre-se: Voce so podera errar",n*5, "vezes.\n")
+    print ("Lembre-se: Você só podera errar",n*5, "vezes.\n")
 
 def random_tudo(mapa):
     l = randint(0, len(mapa) - 1)
-    c = randint(0, len(mapa[0]) - 1)
+    c = randint(0, len(mapa) - 1)
     x=[l,c]
     return x    
 #print (random_tudo(mapa))
@@ -68,14 +86,14 @@ def navio_inteiro(tamanho, mapa_pc):
             else:
                 while x[1]+tamanho > n:
                     x=random_tudo(mapa)
-                if mapa_pc1[x[0]+j][x[1]] == "~":
+                if mapa_pc1[x[0]][x[1]+j] == "~":
                     mapa_pc1[x[0]][x[1]+j] = tamanho
                 else:
                     return navio_inteiro(tamanho,mapa_pc)
         
     except:
         return navio_inteiro(tamanho, mapa_pc)
-#    print (tamanho)    
+    #print (tamanho)    
     return mapa_pc1
         
 
@@ -91,51 +109,57 @@ def posicao(navios):
         
         
 mapa_pc = posicao(navios)
-#print(mapa_pc)
+print(mapa_pc)
 
-erro = 0
+erro = n*5
 rodada = 1
 acertos = 0
 
-while erro < n*5:
+while erro > 0 :
     
     if placar == 41:
-        print ("\nPARABENS! VOCE GANHOU O JOGO!\n")
+        print ("Você afundou todos os meus navios!")        
+        print ("\nPARABÉNS! VOCÊ GANHOU O JOGO!\n")
         break    
 
     print ("\nRodada", rodada,"    Placar:",placar,"\n")
-    print_mapa(mapa)
+    print_mapa(s, mapa)
             
     l_guess = int(input("\nLinha:\n")) -1
+    while l_guess < 0 or l_guess > n-1:
+        print ("\nFora da área! Jogue novamente\n")
+        l_guess = int(input("\nLinha:\n")) -1
+        
     c_guess = int(input("Coluna:\n")) - 1
+    while c_guess < 0 or c_guess > n-1:
+        print ("\nFora da área! Jogue novamente\n")    
+        c_guess = int(input("Coluna:\n")) - 1
+        
     print ("\n-----------------------------------------------------")
     print ("Rodada",rodada, "    Placar:",placar)
 
-    if l_guess < 0 or l_guess > n-1 or c_guess < 0 or c_guess > n-1:
-        print ("\nFora da area! Jogue novamente\n")
-
-    elif(mapa[l_guess][c_guess] == "X" or mapa[l_guess][c_guess] == "o"):
-            print ("\nJa atirou ai! Jogue novamente\n")
-    
+    if(mapa[l_guess][c_guess] == "X" or mapa[l_guess][c_guess] == "o"):
+            print ("\nJá atirou ai! Jogue novamente")
+            
     else:    
         if mapa_pc[l_guess][c_guess] == "~":
-            print ("\nAgua!\n")
+            print ("\nÁgua!")
             mapa[l_guess][c_guess] = "o"
-            erro += 1
+            erro -= 1
     
         elif mapa_pc[l_guess][c_guess] != "~":
-            print ("\nAcertou meu navio!\n")
+            print ("\nAcertou meu navio!")
             barco = mapa_pc[l_guess][c_guess]
             tamanho_barco[barco] -= 1
             
             if tamanho_barco[barco]==0:
-                print ("\nAfundou meu navio!\n")
+                print ("\nAfundou meu navio!")
                 placar += pontos_barcos[barco]
             mapa[l_guess][c_guess] = "X"
     
-    print ("Total de Erros:",erro)
+        if n*n > n*5 + 21:    
+            print ("\nChances de Errar:",erro)
     print ("-----------------------------------------------------\n")
     rodada += 1
     
-print ("\nFIM DE JOGO!\n")
-
+print ("Acabaram suas chances!\n","\nFIM DE JOGO!\n")
